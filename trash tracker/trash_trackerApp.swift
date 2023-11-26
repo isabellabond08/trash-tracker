740f8433c90c82ced1626dba1bac9f7e39ac758c
Isabella Bond <isabellaswag08@gmail.com>
2023-11-25 21:06:04 -0500

    Initial Commit

---
+++ trash tracker/trash_trackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  trash_trackerApp.swift
+//  trash tracker
+//
+//  Created by Viviana Rivera on 11/25/23.
+//
+
+import SwiftUI
+
+@main
+struct trash_trackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
